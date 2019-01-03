88ace2332bf066be051b504ffa6e29b2af37ab0c
BT_1N3_29 <soconnor5921@bths.edu>
2019-01-03 10:09:50 -0500

    1/3

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -11,6 +12,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static sample.GameCode.timer;
 
 public class Main extends Application
@@ -21,6 +25,9 @@ public class Main extends Application
     public static Button blue = new Button();
     public static Button red = new Button();
     public static Button yellow = new Button();
+    public static boolean showing = false;
+    private static long timeStep;
+    private static List<Button> order = new ArrayList<Button>();
 
 
    @Override
@@ -34,7 +41,7 @@ public class Main extends Application
         green.setMinSize(50,50);
         green.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent event) {GameCode.showing = true; GameCode.timer(green);
+            public void handle(ActionEvent event) {showing = true; timer(green);
             }
         });
 
@@ -53,7 +60,7 @@ public class Main extends Application
         Button start = new Button("Start Game");
         start.setOnAction(new EventHandler<ActionEvent>() {
             @Override
-            public void handle(ActionEvent event) {GameCode.createOrder();}
+            public void handle(ActionEvent event) {createOrder();}
         });
 
         HBox hbox1 = new HBox(green, blue, red, yellow, start);
@@ -62,6 +69,64 @@ public class Main extends Application
         primaryStage.setScene(scene1);
         primaryStage.show();
     }
+
+    public static void createOrder()
+    {
+        showing = true;
+        Button btn;
+        btn = pickRandomColor();
+        order.add(btn);
+        timer(btn);
+        level++;
+    }
+
+    public static Button pickRandomColor()
+    {
+        int num = (int)(Math.random()*4);
+        if(num == 0)
+        {
+            return green;
+        }
+        else if(num == 1)
+        {
+            return blue;
+        }
+        else if(num == 2)
+        {
+            return red;
+        }
+        else
+        {
+            return yellow;
+        }
+    }
+
+    public static void timer(Button btn)
+    {
+        timeStep = System.nanoTime() + 100000000L;
+        new AnimationTimer()
+        {
+            public void handle(long now)
+            {
+                if(now > timeStep)
+                {
+                    timeStep = now + 1000000000L;
+                    showing = !showing;
+                }
+                if(showing)
+                {
+                    btn.setStyle("-fx-background-color: lightgreen;");
+                }
+                else
+                {
+                    btn.setStyle("-fx-background-color: green;");
+                    this.stop();
+                }
+
+            }
+        }.start();
+    }
+
 
 
     public static void main(String[] args)
